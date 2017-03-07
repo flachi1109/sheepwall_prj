e964b8a23fd95cea9fb48dadfb9470c8c405fad8
root <root@localhost.localdomain>
2017-03-07 21:57:57 +0800

    modify auxiliay.py

--- sheepwall_app/auxiliarys.py
+++ sheepwall_app/auxiliarys.py
@@ -1,22 +1,37 @@
 # -*- coding:utf-8 -*-
-from models import AppBehaviorLog
 import SocketServer
+import re
+from models import AppBehaviorLog
 
 class SyslogHandler(SocketServer.BaseRequestHandler):
     '''
     Extract key field from syslog message and store in AppBehavior database
     '''
-
-    def handle(self):
-       syslog = self.request[]
-
     def make_syslog_dict(self, syslog_msg):
         syslog_msg_list = syslog_msg.split('')
-        if syslog_msg_list[6].find('AUDIT') > -1:
-            syslog_msg_dict['access_time'] = syslog_msg_list[3]
-            behavior_log =
+        if syslog_msg_list[6].find('AUDIT') > -1:  #Find whether there is keyword 'AUDIT' in the syslog
+
+            #Format behavior_log
+            behavior_log = re.sub(r'\(\d+\)', '', syslog_msg_list[7])
+            behavior_log = re.sub(r'(\w+)=', '"\g<1>\"=', behavior_log)
+            behavior_log = re.sub(r'=([^{}]*?)(;|,)', '=\"\g<1>\"\g<2>', behavior_log)
+            behavior_log = '{'+behavior_log[0:-1].replace(';', ',').replace('=}', '=\"\"}').replace('=',':')+'}'
+
+            #Generate behavior_log dictionary
+            behavior_log_dict = eval(behavior_log)
+            behavior_log_dict['access_time'] = syslog_msg_list[3]  # Save access_time to the dict
+
+            return behavior_log_dict
         else:
             return False
+
+    def handle(self):
+        syslog_msg = self.request[0].strip()
+        behavior_log_dict = self.make_syslog_dict(syslog_msg)
+        if behavior_log_dict:
+            print behavior_log_dict
+
+
 
 
 def start_syslog_server(host, port, syslog_handler):
--- sockservertest.py
+++ sockservertest.py
@@ -1,17 +1,45 @@
 import SocketServer
+import re
+import logging
 
-class MyHandler(SocketServer.BaseRequestHandler):
-	
+class SyslogHandler(SocketServer.BaseRequestHandler):
+	'''
+    Extract key field from syslog message and store in AppBehavior database
+    '''
+
+	def make_syslog_dict(self, syslog_msg):
+		try:
+			syslog_msg_list = syslog_msg.split(' ')
+			if syslog_msg_list[6].find('AUDIT') > -1:  # Find whether there is keyword 'AUDIT' in the syslog
+
+				# Format behavior_log
+				behavior_log = re.sub(r'\(\d+\)', '', syslog_msg_list[7])
+				behavior_log = re.sub(r'(\w+)=', '"\g<1>\"=', behavior_log)
+				behavior_log = re.sub(r'=([^{}]*?)(;|,)', '=\"\g<1>\"\g<2>', behavior_log)
+				behavior_log = '{' + behavior_log[0:-1].replace(';', ',').replace('=}', '=\"\"}').replace('=', ':') + '}'
+
+				# Generate behavior_log dictionary
+				behavior_log_dict = eval(behavior_log)
+				behavior_log_dict['access_time'] = syslog_msg_list[3]  # Save access_time to the dict
+
+				return behavior_log_dict
+			else:
+				return False
+		except ValueError:
+			logging.info('It is not valid syslog message.')
+			return False
+
 	def handle(self):
-		data = self.request[0].strip()
-		socket = self.request[1]
-		print self.request
-		print self.client_address
-		print "{} wrote".format(self.client_address[0])
-		print data
-		socket.sendto(data.upper(), self.client_address)	
+		syslog_msg = self.request[0].strip()
+		behavior_log_dict = self.make_syslog_dict(syslog_msg)
+		if behavior_log_dict:
+			print behavior_log_dict
+
+
+def start_syslog_server(host, port):
+	syslog_server = SocketServer.UDPServer((host, port), SyslogHandler)
+	syslog_server.serve_forever()
 
 if __name__ == '__main__':
-	HOST, PORT = "localhost", 9999
-	server = SocketServer.UDPServer((HOST,PORT), MyHandler)
-	server.serve_forever()
+	host, port = "localhost", 9999
+	start_syslog_server(host, port)
